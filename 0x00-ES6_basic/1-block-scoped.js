efcf4e649c9c8bdd25729dc830a49d3276bb618e
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-06-24 03:52:04 -0700

    Correct scope

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-var */
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+    if (task && task2) {
+      return [task, task2];
+    }
+  }
+
+  return [task, task2];
+}
