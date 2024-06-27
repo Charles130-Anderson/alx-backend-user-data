4bec3318d83bec7c5b2069237dcca9f94a034566
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-06-27 08:45:28 -0700

    Implement Airport class with toString

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,10 +1,20 @@
+// 7-airport.js
+
 export default class Airport {
   constructor(name, code) {
     this._name = name;
     this._code = code;
   }
 
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
   toString() {
-    return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
+    return `[object ${this._code}]`;
   }
 }
