4d5359c124e1861c1c1356045b3f38d0797364ad
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-06-27 05:37:02 -0700

    Initialize ClassRoom size constructor.

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,8 @@
+// Export the ClassRoom class as the default export
+export default class ClassRoom {
+  // Constructor to initialize the maximum size of the class
+  constructor(maxStudentsSize) {
+    // Private property to store the maximum number of students
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
