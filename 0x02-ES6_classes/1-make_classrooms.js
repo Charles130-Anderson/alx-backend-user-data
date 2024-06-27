9c82904ab50cc54d711e78ca15f76839a66fb719
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-06-27 05:50:01 -0700

    Implement initializeRooms function correctly

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,9 @@
-import ClassRoom from './0-classroom';
+import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
-  const sizes = [19, 20, 34];
-  const rooms = sizes.map(size => new ClassRoom(size));
-  return rooms;
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34)
+  ];
 }
