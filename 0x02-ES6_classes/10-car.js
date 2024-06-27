3128dde04b722e22a9b6d85b45f4f50c99920e01
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-06-27 10:00:03 -0700

    Implement Car class with cloneCar.

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,27 +1,18 @@
-const brandSymbol = Symbol('brand');
-const motorSymbol = Symbol('motor');
-const colorSymbol = Symbol('color');
-
 export default class Car {
   constructor(brand, motor, color) {
-    this[brandSymbol] = brand;
-    this[motorSymbol] = motor;
-    this[colorSymbol] = color;
-  }
-
-  get brand() {
-    return this[brandSymbol];
-  }
-
-  get motor() {
-    return this[motorSymbol];
-  }
-
-  get color() {
-    return this[colorSymbol];
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   cloneCar() {
-    return new this.constructor(this.brand, this.motor, this.color);
+    const current = this;
+    // Create a new object with the same prototype as current object
+    const cloned = Object.create(Object.getPrototypeOf(current));
+    // Copy over specific attributes
+    cloned._brand = current._brand;
+    cloned._motor = current._motor;
+    cloned._color = current._color;
+    return cloned;
   }
 }
