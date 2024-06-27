c71a815b81894d02b3a3f4265044c22b0655aa38
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-06-27 10:07:04 -0700

    Add EVCar class override

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -6,11 +6,6 @@ export default class EVCar extends Car {
     super(brand, motor, color);
     this._range = range;
   }
-
-  /**
-   * Override cloneCar method to clone an EVCar instance.
-   * @returns {EVCar} Cloned EVCar instance with the same properties.
-   */
   cloneCar() {
     // Create a new EVCar instance with current attributes
     return new EVCar(this._brand, this._motor, this._color, this._range);
