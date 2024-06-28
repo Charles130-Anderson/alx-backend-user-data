8eb1eaaca2eeb91183abf1a9cd01d5a76f5125f5
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-06-28 12:46:24 -0700

    Implement createReportObject

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,11 +1,14 @@
+// This module exports a function that creates a report object
+// The report object includes a list of all employees and a method to get the number of departments
+
 export default function createReportObject(employeesList) {
   return {
-    allEmployees: { 
+    allEmployees: {
       ...employeesList,
     },
-
     getNumberOfDepartments(employeesList) {
       return Object.keys(employeesList).length;
     },
   };
 }
+
