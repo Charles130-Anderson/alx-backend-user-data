c13b36a8543502bc8f44482cb27c5b47578f4db3
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-07-02 06:09:32 -0700

    Add function to list students

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,12 @@
+// Define a function that returns an array of student objects
 const getListStudents = () => [
+  // Student 1
   { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  // Student 2
   { id: 2, firstName: 'James', location: 'Columbia' },
+  // Student 3
   { id: 5, firstName: 'Serena', location: 'San Francisco' },
 ];
 
+// Export the function as the default export
 export default getListStudents;
