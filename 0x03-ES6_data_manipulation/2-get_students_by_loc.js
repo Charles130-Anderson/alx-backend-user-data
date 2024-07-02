c21f4e6a5dbf030b81b79a70bec0a58ea94d497a
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-07-02 07:04:07 -0700

    Extract student IDs from array

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,10 @@
-const getStudentsByLocation = (students, city) =>
-  Array.isArray(students) ? students.filter(student => student.location === city) : [];
-
-export default getStudentsByLocation;
+// Function to export as default
+export default function getStudentIdsSum(students) {
+  // Check if input is an array
+  if (Array.isArray(students)) {
+    // Use reduce to sum up ids in the array of objects
+    return students.reduce((accumulator, each) => accumulator + each.id, 0);
+  }
+  // Return an empty array if input is not an array
+  return [];
+}
