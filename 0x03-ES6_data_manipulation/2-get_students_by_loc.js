a5653900b0bbbcc42854864e95962437cb2580ae
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-07-02 09:02:02 -0700

    Filter students by location

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,10 +1,4 @@
-// Function to export as default
-export default function getStudentIdsSum(students) {
-  // Check if input is an array
-  if (Array.isArray(students)) {
-    // Use reduce to sum up ids in the array of objects
-    return students.reduce((accumulator, each) => accumulator + each.id, 0);
-  }
-  // Return an empty array if input is not an array
-  return [];
+export default function getStudentsByLocation(students, city) {
+
+  return students.filter((student) => student.location.localeCompare(city) === 0);
 }
