1e3879a6292e6869710a9133dbeacd66c665c7f2
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-08-08 02:55:30 -0700

    Add GET /users/me endpoint

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+API authentication module
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Authentication """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Determines if authentication is required """
+        if path is None or not excluded_paths:
+            return True
+        for i in excluded_paths:
+            if i.endswith('*') and path.startswith(i[:-1]):
+                return False
+            elif i in {path, path + '/'}:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Retrieves authorization header """
+        if request is None:
+            return None
+
+        return request.headers.get("Authorization", None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Retrieves the current user """
+        return None
