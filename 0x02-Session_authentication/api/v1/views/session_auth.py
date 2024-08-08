237080c643f9ee86d303d514a08d3f1baee12b65
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-08-08 08:10:27 -0700

    Implement session-based login route

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Import Flask from flask module
+"""
+from flask import request, jsonify, session
+from models.user import User
+from api.v1.views import app_views
+from os import getenv
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def session_login() -> str:
+
+    """Auth session"""
+
+    user_email = request.form.get('email')
+    user_pswd = request.form.get('password')
+
+    if not user_email:
+        return jsonify({"error": "email missing"}), 400
+    if not user_pswd:
+        return jsonify({"error": "password missing"}), 400
+
+    try:
+        search_users = User.search({'email': user_email})
+    except Exception:
+        return jsonify({"error": "no user found for this email"}), 404
+    if not search_users:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    user = search_users[0]
+    if not user.is_valid_password(user_pswd):
+        return jsonify({"error": "wrong password"}), 401
+    from api.v1.app import auth
+    session_cookie = getenv("SESSION_NAME")
+    session_id = auth.create_session(user.id)
+    response = jsonify(user.to_json())
+    response.set_cookie(session_cookie, session_id)
+    return response
