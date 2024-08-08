c4c647dd4ef79d813f45996f3bf73c0cb82f251b
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-08-08 09:29:25 -0700

    Implement session storage in database.

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,41 +1,27 @@
 #!/usr/bin/env python3
-"""SessionDBAuth class"""
+"""
+API session db module
+"""
+
 from api.v1.auth.session_exp_auth import SessionExpAuth
-from models.user_session import UserSession
-from datetime import datetime
+from os import getenv
 
 
 class SessionDBAuth(SessionExpAuth):
-    """SessionDBAuth class"""
-    def create_session(self, user_id=None):
-        """Create session"""
-        session_id = super().create_session(user_id)
-        if not session_id:
-            return None
-        user_id = UserSession(user_id=user_id, session_id=session_id)
-        user_id.save()
-        return session_id
+    """ Session DB Auth """
 
-    def user_id_for_session_id(self, session_id=None):
-        """Retrieves user id using session id"""
-        if session_id is None:
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates a Session ID for user_id """
+        pass
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Returns User ID based on Session ID """
+
+        if session_id is None or isinstance(session_id, str) is False:
             return None
-        if not isinstance(session_id, str):
-            return None
-        user_id = UserSession.get(session_id)
-        if not user_id:
-            return None
-        return user_id
+        else:
+            pass
 
     def destroy_session(self, request=None):
-        """Destroy session"""
-        if request is None:
-            return False
-        session_id = self.session_cookie(request)
-        if not session_id:
-            return False
-        user_id = self.user_id_for_session_id(session_id)
-        if not user_id:
-            return False
-        user_id.delete()
-        return True
+        """ Deletes user session to logout """
+        pass
