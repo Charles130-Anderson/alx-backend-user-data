226a0f02cad0f4577b010d2dff8b2a5adf4d6302
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-08-14 00:24:27 -0700

    Set up basic Flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Basic Flask application"""
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def hello_world() -> str:
+    """Return welcome message in JSON"""
+    return jsonify({"message": "Bienvenue"})
+
+if __name__ == "__main__":
+    """Run the Flask app"""
+    app.run(host="0.0.0.0", port="5000")
