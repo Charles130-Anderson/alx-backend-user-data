5ae84ca429695fac21886f48562d306f5dd7587d
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-08-14 01:28:46 -0700

    Add UUID generation utility function.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -47,6 +47,7 @@ def _hash_password(password: str) -> bytes:
     """Hash a password using bcrypt."""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
+
 def _generate_uuid() -> str:
     """Returns a string representation of a new UUID"""
     UUID = uuid4()
