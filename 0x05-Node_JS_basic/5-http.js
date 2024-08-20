cdbf4583b6c7b1a90d7df9d2e3198d3064a55319
Charles130-Anderson <andersonoyoo034@gmail.com>
2024-08-20 02:42:15 -0700

    Implement HTTP server with routes.

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,38 +1,24 @@
 const http = require('http');
 const students = require('./3-read_file_async');
-
 const hostname = '127.0.0.1';
 const port = 1245;
 
-/**
- * Creates and exports an HTTP server.
- * @type {http.Server}
- */
-const app = http.createServer(async (req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     res.write('This is the list of our students\n');
-    
-    try {
-      const data = await students(process.argv[2]);
+    students(process.argv[2]).then((data) => {
       res.write(`Number of students: ${data.students.length}\n`);
       res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
       res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
-    } catch (err) {
-      res.write(err.message);
-    }
-    
-    res.end();
-  } else {
-    res.statusCode = 404;
-    res.end('Not Found');
+      res.end();
+    }).catch((err) => res.end(err.message));
   }
 });
-
+  
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}`);
 });
